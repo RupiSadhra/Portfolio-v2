a310bcc9fc67dec7d1a74008b2f26250c21ff46f
Rupi Sadhra <info@rupicodes.com>
2021-05-24 10:27:03 +1200

    Portfolio-v1 files added

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rupi Sadhra</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+</head>
+<body>
+    
+        <section class="hero overlay-container">
+
+            <div class="overlay overlay-hero"></div>
+
+            <div class="hero-section container-width center-element">
+                <div class="first-name"><h1>Rupi</h1></div>
+                <div class="last-name"><h1>Sadhra</h1></div>
+                <section class="link-tags">
+                    <a target="_blank" href="https://github.com/RupiSadhra"><i class="fab fa-github"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/rupi-sadhra-23aa55148/"><i class="fab fa-linkedin-in"></i></a>
+           
+                </section>
+           </div>
+
+           <div class="pyramid"></div>
+        </section>
+
+
+
+
+
+
+
+        <section class="black-section center-element">
+           <!-- <div class="overlay overlay-right"></div> -->
+
+            <div class="about-intro center-element">
+           
+                        <p class="margin-heading padding-bottom">
+                            Hello I'm Rupi Sadhra, a Web Developer. I have a year experience of developing custom websites that are responsive and user friendly. Apart from coding my another skill is cooking.
+                        </p>
+                 
+                   
+                
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+        <section class="about overlay-container skill-description">
+            <!-- <div class="overlay overlay-skills"></div> -->
+            <div class="container-width center-element">
+                <!-- <div id="move-right"class=" padding-top"><h1>Skills</h1></div> -->
+                <div id="move-right"class="margin-top-small"><h1>Skills</h1></div>
+                
+            </div>
+
+             <div class="container-width margin-heading margin-bottom center-element">
+                <!-- <div class="two-column-grid margin-top-extra grid-gap-extra">
+                    <section class="skill-line">
+                        <p>I love to learn new technologies and languages to add to my skill set. At the moment I am obsessed with React.</p>
+                    </section>
+                    
+                    <section class="skill-set">
+                        <span>HTML</span>
+                        <span>CSS</span>
+                        <span>JAVASCRIPT</span>
+                        <span>CSS GRID</span>
+                        <span>BOOTSTRAP</span>
+                        <span>PHP</span>
+                        <span>MYSQL</span>
+                        <span>SHOPIFY</span>
+                    </section>
+                </div> -->
+                <div class="skills">
+                    <img class="html-image"src="media/html.png" alt="" width="100" height="100">
+                    <img class="css-image"src="media/css.png" alt="" width="70" height="100">
+                    <img class="js-image"src="media/js.png" alt="" width="140" height="100">
+                    <img class="bs-image"src="media/bs.png" alt="" width="150" height="150">
+                    <img class="php-image"src="media/php.png" alt="" width="100" height="80">
+                    <img class="grid-image"src="media/grid.svg" alt="" width="100" height="100">
+                    <img class="shopify-image"src="media/shopify.png" alt="" width="150" height="100">
+                    <img class="sql-image"src="media/mysql.png" alt="" width="120"  height="100">
+                </div>
+               
+                
+            </div>
+
+           
+        </section>
+    
+
+
+
+
+
+
+
+    <section class="black-section margin-top">
+            <div class="container-width center-element">
+            
+                <div id="move-right"class="margin-top"><h1>Portfolio</h1></div>
+                
+            </div>
+            <!-- project one -->
+            <div class="portfolio-description   project-one-trigger two-column-grid margin-top container-width grid-gap-extra">
+                <div class="text-uppercase">Pwrfit Studios</div>
+                <section>
+                    <span>Pwrfit studios is a boutique fitness studio where you can nourish, train and relax. The signed up users can book classes online.</span>
+                    <br><br>
+                    <a href="https://www.pwrfitstudio.com/" target="_blank"class="button-style">Visit Website</a>
+                   
+                </section>
+            </div>
+
+            <div class="portfolio-image margin-top">
+                <img class="background-display" src="media/pwrfit.png" alt="" width="1000" height="800">
+                
+                <div class="portfolio-overlay-pwrfit">
+                    <img class="project-one"src="media/pwrfit-website-bg.png" alt="" width="1000" height="800">
+                </div>
+            </div>
+
+
+
+
+
+             <!-- project two -->
+             <div class="margin-top portfolio-description   project-two-trigger two-column-grid container-width grid-gap-extra">
+                <div class="text-uppercase">Digicreate</div>
+                <section>
+                    <span>Digicreate helps businesses into valuable, purpose-filled brands through a strong visual presence and develop websites.</span>
+                    <br><br>
+                    <a href="http://rupicodes.com/digicreate/" class="button-style" target="_blank">Visit Website</a>
+                
+                </section>
+            </div>
+
+            <div class="portfolio-image margin-top">
+                <img class="background-display" src="media/digicreate.png" alt="" width="1000" height="800">
+                
+                <div class="portfolio-overlay">
+                    <img class="project-two"src="media/digicreate-full.png" alt="" width="1000" height="800">
+                </div>
+            </div>
+
+
+
+
+            <div class="margin-top portfolio-description   project-three-trigger two-column-grid container-width grid-gap-extra">
+                <div class="text-uppercase">The Kitchen Catering</div>
+                <section>
+                    <span>The Kitchen Catering is a catering company that provides perfect solution at the right price, including cheap catering options for smaller budgets.</span>
+                    <br><br>
+                    <a href="http://www.kitchencatering.co.nz/" target="_blank"class="button-style">Visit Website</a>
+                    
+                </section>
+            </div>
+
+            <div class="portfolio-image margin-top overflow-hidden">
+                <img class="background-display" src="media/kitchen.png" alt="" width="1000" height="800">
+                
+                <div class="portfolio-overlay">
+                    <img class="project-three"src="media/catering.png" alt="" width="1000" height="800">
+                </div>
+            </div>
+
+
+             <div class="margin-top portfolio-description   project-four-trigger two-column-grid container-width grid-gap-extra">
+                <div>Webgo</div>
+                <section>
+                <span>Webgo is my personal project for which I am practising on page search engine optimisation to gain knowledge how google search engine ranks websites.</span>
+                    <br><br>
+                    <a href="https://webgo.co.nz/" target="_blank"class="button-style">Visit Website</a>
+                    
+                </section>
+            </div>
+
+            <div class="portfolio-image margin-top overflow-hidden">
+                <img class="background-display" src="media/webgo-bg.png" alt="" width="1000" height="800">
+                
+                <div class="portfolio-overlay">
+                    <img class="project-four"src="media/web.png" alt="" width="1000" height="800">
+                </div>
+            </div>
+
+
+
+            <div class="margin-top portfolio-description   project-five-trigger two-column-grid container-width grid-gap-extra">
+                <div class="text-uppercase">Little Nostalgic</div>
+                <section>
+                <span>Little Nostalgic is an online ecommerce Shopify vintage clothing store for kids with a mission to minimise the waste generated by fast fashion, by rehoming pieces from the past love. </span>
+                    <br><br>
+                    <a href="https://littlenostalgic.com/" target="_blank" class="button-style">Visit Website</a>
+                   
+                </section>
+            </div>
+
+            <div class="portfolio-image margin-top overflow-hidden">
+                <img class="background-display" src="media/shopfy-bg.png" alt="" width="1000" height="800">
+                
+                <div class="portfolio-overlay">
+                    <img class="project-five"src="media/liitle.png" alt="" width="1000" height="800">
+                </div>
+            </div>
+
+
+            
+
+    </section>
+
+
+
+    <section class="hero center-element">
+        <div class="container-width-extra center-element">
+            <p><a class="email-link"href="mailto:info@rupicodes.com">info@rupicodes.com</a></p>
+            <p>0223954408</p>
+        </div>
+    </section>
+    
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/ScrollTrigger.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/MotionPathPlugin.min.js" integrity="sha512-Dbub1kAP2vz+A+B+SnoJ/d79wY/Lxj/4c5A2LDm8sCF2YWHGJJRYNxDCJLP56cCsVfzhVZMMqPbGDx8ckvdt5g==" crossorigin="anonymous"></script>
+    <script src="javascript/scrollAnimation.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ javascript/scrollAnimation.js
@@ -0,0 +1,442 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(MotionPathPlugin);
+
+
+
+
+    var tl = gsap.timeline();
+
+    tl.to(".first-name",{
+        x:150,
+        opacity:0.5,
+        duration:1,
+        yoyo:true,
+        repeat:1
+    });
+    
+    
+    
+    tl.to(".last-name",{
+        x:-150,
+        opacity:0.5,
+        duration:1,
+        yoyo:true,
+        repeat:1
+    },"-=2"
+    );
+
+    tl.to('.overlay',{
+      height:document.querySelector('.hero').offsetHeight,
+        duration:1
+    }
+    );
+
+
+
+
+gsap.to(".first-name",{
+    x:-150,
+    opacity:0.5,
+    duration:4,
+    scrollTrigger:{
+        trigger:".hero",
+        start:"top",
+        end:"bottom 80%",
+        scrub:2,
+    }
+});
+
+
+
+gsap.to(".last-name",{
+    x:150,
+    opacity:0.5,
+    duration:4,
+    scrollTrigger:{
+        trigger:".hero",
+        start:"top",
+        end:"bottom 80%",
+        scrub:2
+    }
+});
+
+// gsap.to("#move-right",{
+//     x:450,
+//     opacity:0.5,
+//     duration:6,
+//     scrollTrigger:{
+//         trigger:".skills",
+//         start:"",
+//         end:"bottom 80%",
+//         scrub:2,
+//     }
+// });
+
+
+// gsap.to("#move-left",{
+//     x:-350,
+//     opacity:0.5,
+//     duration:6,
+//     scrollTrigger:{
+//         trigger:".skills",
+//         start:"",
+//         end:"bottom 80%",
+//         scrub:2
+//     }
+// });
+
+
+gsap.to(".pyramid",{
+    rotate:360,
+   opacity:1,
+    // repeat:-1,
+    // yoyo:true,
+    motionPath:{
+        path:[
+             {x:10, y:200}, 
+            {x:50, y:250}, 
+            {x:100, y:280}, 
+            {x:150, y:290}, 
+            {x:180, y:300}, 
+            {x:200, y:400}, 
+            {x:500, y:600},
+            {x:300, y:650},
+            {x:100, y:700},
+           {x:-200,y:800},
+           {x:-300,y:850},
+            {x:-600,y:1000},
+            {x:0,y:1200},
+            // {x:-100,y:1200},
+     
+            // {x:-800,y:1550},
+            //    {x:0,y:1200},
+            //  {x:-100,y:1300},
+            //  {x:-600,y:1800},
+            //  {x:-400,y:1850},
+            //  {x:200,y:1900},
+            //  {x:0,y:2050},
+            //  {x:800,y:2200},
+            //  {x:-300,y:2500},
+            //  {x:-500,y:2600},
+            //  {x:-700,y:2800},
+            //  {x:0,y:3000},
+            //  {x:200,y:3500},
+            //  {x:400,y:3560},
+            //   potfolio
+            //   {x:-500,y:3400},
+            //   {x:0,y:3600},
+             
+            //  {x:-500,y:3650},
+            //    {x:0,y:3700},
+            //   {x:600,y:3800},
+            //   {x:650,y:4200},
+            //   {x:-400,y:4000},
+            //  {x:-600,y:4500},
+            //   {x:-200,y:5000},
+            // {x:-400,y:5300},
+            //  {x:-600,y:5800},
+            //  {x:0,y:6000},
+            //  {x:200,y:6400},
+            //  {x:400,y:6850},
+           
+            //  {x:-300,y:7500},
+            //  {x:0,y:7800},
+            //  {x:100,y:8400},
+        ]
+    } ,
+    scrollTrigger:{
+      
+        trigger:".hero",
+        start:"top",
+        endTrigger:".about-intro",
+        end:"bottom",
+        scrub:1,
+    }
+    
+});
+
+// hero overlay hide left
+
+gsap.to('.overlay-left',{
+    x:-document.querySelector('.overlay').offsetWidth,
+    duration:1,
+    scrollTrigger:{
+        trigger:".overlay-left",
+        start:"top",
+        end:"bottom",
+        scrub:0.5
+    }
+});
+
+gsap.to('.overlay-skills',{
+    x:-document.querySelector('.overlay').offsetWidth,
+    duration:1,
+    scrollTrigger:{
+        trigger:".overlay-skills",
+        start:"top",
+        end:"bottom",
+        scrub:0.5
+    }
+})
+
+gsap.to('.overlay-right',{
+    x:document.querySelector('.overlay').offsetWidth,
+    duration:1,
+    scrollTrigger:{
+        trigger:".overlay-right",
+        start:"top",
+        end:"bottom",
+        scrub:0.5
+    }
+})
+
+
+//project images move right
+// gsap.to("#portfolio-one img",{
+//     x:document.querySelector('#portfolio-one').offsetWidth,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#portfolio-one",
+//         start:"top",
+//         end:"bottom",
+//         scrub:0.5,
+     
+//         ease:Circ.easeOut
+//     }
+// });
+
+// gsap.to("#portfolio-two img",{
+//     x:document.querySelector('#portfolio-two').offsetWidth,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#portfolio-two",
+//         start:"top",
+//         end:"bottom",
+//         scrub:0.5,
+       
+//         ease:Circ.easeOut
+//     }
+// });
+
+// gsap.to("#portfolio-three img",{
+//     x:document.querySelector('#portfolio-three').offsetWidth,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#portfolio-three",
+//         start:"top",
+//         end:"bottom",
+//         scrub:0.5,
+        
+//         ease:Circ.easeOut
+//     }
+// });
+
+// let sections = gsap.utils.toArray(".panel");
+
+// gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: ".horizontal-container",
+//       pin: true,
+//       pinSpacing:false,
+//        scrub: 0.8,
+//       snap: 1 / (sections.length - 1),
+//       markers:true,
+//       // base vertical scrolling on how wide the container is so it feels more natural.
+//       end: () => "+=" + document.querySelector(".horizontal-container").offsetWidth
+//     }
+//   });
+
+
+//   skills images
+
+gsap.to('.html-image',{
+    duration:1,
+    x:-50,
+    y:-50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top",
+        end:"bottom",
+        scrub:1
+        
+    }
+})
+gsap.to('.php-image',{
+    duration:1,
+    x:-50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top",
+        end:"bottom",
+        scrub:1
+        
+    }
+})
+gsap.to('.grid-image',{
+    duration:1,
+    x:-50,
+    y:50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top 20%",
+        end:"bottom",
+        scrub:1
+    }
+})
+
+gsap.to('.css-image',{
+    duration:1,
+    x:50,
+    y:-50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top 20%",
+        end:"bottom",
+        scrub:1
+        
+    }
+})
+gsap.to('.bs-image',{
+    duration:1,
+    x:50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top 20%",
+        end:"bottom",
+        scrub:1
+        
+    }
+})
+gsap.to('.shopify-image',{
+    duration:1,
+    x:50,
+    y:50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top 20%",
+        end:"bottom",
+        scrub:1
+    }
+})
+
+gsap.to('.js-image',{
+    duration:1,
+    y:-50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top 20%",
+        end:"bottom",
+        scrub:1
+        
+    }
+})
+gsap.to('.sql-image',{
+    duration:1,
+   
+    y:50,
+    scrollTrigger:{
+        trigger:'.skills-description',
+        start:"top 20%",
+        end:"bottom",
+        scrub:1
+    }
+})
+
+
+// portfolio images
+
+gsap.to('.project-one',{
+    duration:5,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.project-one-trigger',
+        start:"top 2%",
+        end:"bottom 100%",
+        scrub:3,
+       
+    }
+})
+
+gsap.to('.project-two',{
+    duration:5,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.project-two-trigger',
+        start:"top 2%",
+        scrub:3,
+        end:"bottom 100%"
+    }
+})
+
+gsap.to('.project-three',{
+    duration:5,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.project-three-trigger',
+        start:"top 2%",
+        end:"bottom 100%",
+        scrub:3,
+      
+    }
+})
+
+gsap.to('.project-four',{
+    duration:5,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.project-four-trigger',
+        start:"top 2%",
+        end:"bottom 100%",
+        scrub:3,
+      
+    }
+})
+
+
+gsap.to('.project-five',{
+    duration:5,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.project-five-trigger',
+        start:"top 2%",
+        end:"bottom 100%",
+        scrub:3,
+       
+    }
+})
+gsap.to('.skill-line',{
+    duration:2,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.skill-description',
+        start:"top 2%",
+        end:"bottom 100%",
+        scrub:4,
+       
+    }
+})
+gsap.to('.skill-set',{
+    duration:2,
+   
+    y:-100,
+    scrollTrigger:{
+        trigger:'.skill-description',
+        start:"top 2%",
+        end:"bottom 100%",
+        scrub:4,
+       
+    }
+})
+
+
+
+  
+  
